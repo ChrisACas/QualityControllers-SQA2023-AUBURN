d50387d92a89dab848b77bc078854a90e676a8fd
Nihitha Reddy S <nihitha40633@gmail.com>
2023-05-01 08:51:23 -0500

    Software Forensic - logging - Nihitha Reddy S

--- KubeSec-master/logging_forensic.py
+++ KubeSec-master/logging_forensic.py
@@ -1,7 +1,6 @@
+#creates and returns a logging object with a specific format and file name.
 import logging 
 
-
-#creates a logging object and returns it
 def giveMeLoggingObject():
     format_str = '%(name)s-%(asctime)s - %(process)d -%(levelname)s- %(message)s'
     file_name  = 'logger.log' #logs are stored in this file
